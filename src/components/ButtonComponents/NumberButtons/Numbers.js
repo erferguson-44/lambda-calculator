58f1b5a9f1c969ea2d4865e768d5a843e4542176
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-09-10 12:00:53 -0700

    STEP 1.1 and 1.2 -- imported COMPONENTS to N.js, O.js,S.js

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 //import any components needed
-// example of import from data.js. Note all the ../   This is how we move through folders. 
-/* 
-import { numbers } from '../../../data' 
-*/
+import NumberButton from "./NumberButton"
+
 //Import your array data to from the provided data file
+// example import { numbers } from '../../../data' 
+import { numbers } from "../../../data"
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
